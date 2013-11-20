0c429fca0c6efdbbeb2e95b4b45320f254b23b39
Matthieu Rosinski <korrigan@staff.42.fr>
2013-11-20 12:37:33 +0100

    improved pfstatelog

--- pfstatelog.py
+++ pfstatelog.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
+import sys
 import pcapy
+from datetime import datetime
+
 from pfsync.packet import Reader
 
 def recv_pkt(pcap_hdr, data):
     print "-- Received packet --"
     (ts, ms) = pcap_hdr.getts()
-    print "%s.%d" % (str(datetime.from_timestamp(ts)), ms)
+    print "%s.%d" % (str(datetime.fromtimestamp(ts)), ms)
     r = Reader(data)
     r.dump()
     print "-- END --\n\n"
 
+def usage():
+    print "Usage: pfstatelog.py <pfsync interface>"
+    sys.exit(1)
+
 if __name__ == '__main__':
-    r = pcapy.open_live('en0', 1600, False, 100)
+    if len(sys.argv) < 2:
+        usage()
+    r = pcapy.open_live(sys.argv[1], 1600, False, 100)
     r.loop(-1, recv_pkt)
+    sys.exit(0)
