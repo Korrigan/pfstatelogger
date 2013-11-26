8ac1b584bebb8fee8a53430b1fa3580be85dc9e2
Matthieu Rosinski <korrigan@staff.42.fr>
2013-11-26 15:56:07 +0100

    refactoring manager (closes #110)

--- pfstatelog.py
+++ pfstatelog.py
@@ -4,41 +4,7 @@ import sys
 import pcapy
 from datetime import datetime
 
-from pfsync.packet import Reader
-
-class StateManager(object):
-    """
-    This class is used to manage states
-    It keep a state table in memory and log messages when needed
-
-    """
-    def __init__(self):
-        self.states = []
-
-    def add_state(self, state):
-        """
-        Add a state and log it
-        Should implements a way to check if not already in list
-
-        direction == 2 corresponds to the #define PF_OUT
-        See OpenBSD sources sys/net/pfvar.h
-
-        """
-        if state.direction == 2:
-            self.states.append(state)
-            print "INS STATE: %s" % str(state)
-
-    def del_state(self, state):
-        """
-        Delete state by id
-        Works with pfsync messages del and del_c
-
-        """
-        for s in self.states[:]:
-            if s.id == state.id:
-                print "DEL STATE: %s" % str(s)
-                self.states.remove(s)
-
+from pfsync.packet import Reader, StateManager
 
 manager = StateManager()
 
@@ -47,23 +13,13 @@ def recv_pkt(pcap_hdr, data):
     (ts, ms) = pcap_hdr.getts()
     r = Reader(data)
     for a in r.actions:
-        f = None
-        if a.is_add_action():
-            f = manager.add_state
-        elif a.is_del_action():
-            f = manager.del_state
-        if f:
-            for m in a.messages:
-                f(m)
+        manager.handle_action(a)
 
-#    print "-- Received packet --"
-#    print "%s.%d" % (str(datetime.fromtimestamp(ts)), ms)
-#    r.dump()
-#    print "-- END --\n\n"
 
 def usage():
     print "Usage: pfstatelog.py <pfsync interface>"
     sys.exit(1)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
--- pfsync/packet.py
+++ pfsync/packet.py
@@ -33,3 +33,73 @@ class Reader(object):
             print "--"
             a.dump()
             print "--"
+
+
+class StateManager(object):
+    """
+    This class is used to manage states
+    It keep a state table in memory and log messages when needed
+
+    """
+
+    def __init__(self):
+        self.states = []
+        self.handles = [
+            self._clr_states,
+            None,
+            None,
+            None,
+            None,
+            None,
+            self._del_state,
+            self._del_state,
+            None,
+            None,
+            None,
+            None,
+            None,
+            self._add_state,
+            None,
+            None,
+            ]
+
+    def handle_action(self, action):
+        """
+        Handle an action class and update the state table in this way
+        This method call the right internal method for each message of
+        the action class.
+
+        """
+        id = action.header.action_id
+        if id >= 0 and id < len(self.handles) and self.handles[id]:
+            for m in action.messages:
+                return self.handles[id](m)
+
+    def _clr_states(self, msg):
+        """
+        Clear all states on a specified interface by a specific creator
+        Handles pfsync message clr
+
+        """
+        print "CLR STATES: %s" % str(msg)
+
+    def _add_state(self, state):
+        """
+        Add a state and log it
+        Should implements a way to check if not already in list
+
+        direction == 2 corresponds to the #define PF_OUT
+        See OpenBSD sources sys/net/pfvar.h
+
+        """
+        if state.direction == 2:
+            print "INS STATE: %s" % str(state)
+
+    def _del_state(self, state):
+        """
+        Delete state by id
+        Works with pfsync messages del and del_c
+
+        """
+        print "DEL STATE: %s" % str(state)
+
